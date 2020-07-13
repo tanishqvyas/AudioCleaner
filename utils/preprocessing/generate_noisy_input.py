92d0c38afa2a24aff36623797d2b7f653702767a
tanishqvyas <tanishqvyas069@gmail.com>
2020-07-13 13:37:18 +0530

    Audio merger added

--- utils/preprocessing/generate_noisy_input.py
+++ utils/preprocessing/generate_noisy_input.py
@@ -5,8 +5,10 @@ This file aims at getting the clean input audio and mixing it with a noise audio
 
 """
 
-from scipy.io import wavfile
 import os
+from scipy.io import wavfile
+from pydub import AudioSegment
+
 
 
 class InputGenerator:
@@ -22,18 +24,19 @@ class InputGenerator:
 	# Function to fetch all the audio files from a folder
 	# Argument is the folder path
 	# Return a list of all the files
-	def fetch_all_files(self, path):
+	def fetch_all_file_names(self, path):
 		
 		audio_list = []
 
 		for filename in os.listdir(path):
 
 			# Sampling rate in samples per sec, numpy array of data
-			fs, data = wavfile.read(os.path.join(path, filename))
+			# fs, data = wavfile.read(os.path.join(path, filename))
 			
 			# checking if return value is valid
-			if data is not None:
-				audio_list.append([fs,data])
+			# if data is not None:
+				# audio_list.append([fs,data])
+			audio_list.append(filename)
 
 		return audio_list
 
@@ -43,19 +46,29 @@ class InputGenerator:
 
 
 	# Function to generate noisy input data
-	def generate_noisy_input(self):
+	def generate_noisy_input(self, sample_count=1):
 		
 		# List of final noisy input data
 		input_data_list = []
 
 		# Get all the clean audios
-		clean_audio_list = self.fetch_all_files(self.clean_path)
+		clean_audio_list = self.fetch_all_file_names(self.clean_path)
+
+		# Get all the noise audios
+		noise_audio_list = self.fetch_all_file_names(self.noise_path)
+
 
 		# Iterate over each individual audio and generate corresponding noisy input
-		for audio in clean_audio_list:
-			pass
+		for i in range(sample_count):
+			
+			original_audio = AudioSegment.from_file(os.path.join(self.clean_path, clean_audio_list[i]))
+			noise_audio    = AudioSegment.from_file(os.path.join(self.noise_path, noise_audio_list[i]))
 
-		return input_data_list
+			combined = original_audio.overlay(noise_audio)
+
+			combined.export(os.path.join(self.input_path, clean_audio_list[i]), format='wav')
+
+
 
 
 
