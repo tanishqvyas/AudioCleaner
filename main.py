4a9c7a4b8a625f9441390ebc9d298845ca3e9ceb
tanishqvyas <tanishqvyas069@gmail.com>
2020-07-14 11:36:27 +0530

    Added .wav file plotting function

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from utils.preprocessing.generate_noisy_input import InputGenerator
 clean_audio_path = os.path.join("data", "clean")
 noise_audio_path = os.path.join("data", "noise")
 input_audio_path = os.path.join("data", "input")
+audio_plot_path  = os.path.join("data", "plots")
 
 
-obj = InputGenerator(clean_audio_path, noise_audio_path, input_audio_path)
+obj = InputGenerator(clean_audio_path, noise_audio_path, input_audio_path, audio_plot_path)
 
-obj.generate_noisy_input()
+# obj.generate_noisy_input()
 
 # obj.play_audio(os.path.join(clean_audio_path, "name.wav"))
+
+obj.plot_the_audio(clean_audio_path, "name.wav")
--- utils/preprocessing/generate_noisy_input.py
+++ utils/preprocessing/generate_noisy_input.py
@@ -8,6 +8,12 @@ This file aims at getting the clean input audio and mixing it with a noise audio
 import os
 
 
+from matplotlib import pyplot as plt
+import numpy as np
+import wave
+import sys
+
+
 from scipy.io import wavfile
 from pydub import AudioSegment
 from pydub.playback import play
@@ -16,11 +22,12 @@ from pydub.playback import play
 
 class InputGenerator:
 
-	def __init__(self, clean_path, noise_path, input_path, distortion = 0.1):
+	def __init__(self, clean_path, noise_path, input_path, plot_path, distortion = 0.1):
 
 		self.clean_path = clean_path
 		self.noise_path = noise_path
 		self.input_path = input_path
+		self.plot_path  = plot_path
 		self.distortion = distortion
 
 
@@ -73,6 +80,25 @@ class InputGenerator:
 
 		sound = AudioSegment.from_file(path, format="wav")
 		play(sound)
+
+	# A function to plot the audio
+	def plot_the_audio(self, folder_path, filename, wanna_save = False):
+
+		samplerate, data = wavfile.read(os.path.join(folder_path, filename))
+
+		duration = len(data)/samplerate
+		time = np.arange(0,duration,1/samplerate) #time vector
+
+		plt.plot(time,data)
+		plt.xlabel('Time [s]')
+		plt.ylabel('Amplitude')
+		plt.title(filename)
+		fig = plt.gcf()
+		plt.show()
+
+		if(wanna_save):
+
+			fig.savefig(os.path.join(self.plot_path, filename + ".png"))
 
 
 
